105949a991dd5a6629b148f3b5e5213ab0aeaf71
Alejandro Caldi <alejandro.caldi@gmail.com>
2025-02-28 18:45:38 +0100

    Cambios en inventario y Jvados en todo el proyecto. Borrado de la main dummy.

--- src/inventario/Producto.java
+++ src/inventario/Producto.java
@@ -1,11 +1,26 @@
 package inventario;
 
+/**
+ * 
+ * @author alejandro César Caldi
+ * @version 1.6.0
+ * @since 2025-02-28
+ * @see Tarea par aunidad 4 de la materia Entornos de Desarrollo.
+ */
 public class Producto {
+    
     private String nombre;
     private double precio;
     private int stock;
 
-     // Método de fábrica estático
+    /**
+     * Constructor de fábrica estático. Invoca al constructor privado para la creación de un producto. 
+     * 
+     * @param nombre String con el nombre del producto. 
+     * @param precio double con el precio del producto. 
+     * @param stock int con la cantidad del producto en stock 
+     * @return una instancia de Producto. 
+     */
     public static Producto crearProducto(String nombre, double precio, int stock) {
        
         return new Producto(nombre, precio, stock);
@@ -13,6 +28,14 @@ public class Producto {
     }
     
     // Constructor privado de fábrica
+    /**
+     * Constructor privado de la clase Produtco. 
+     * 
+     * @param nombre String el nombre del producto.
+     * @param precio double el precio delproducto. 
+     * @param stock int con la cantida de stock del gobierno. 
+     * @throws no hay excepciones a manejar. 
+     */
     private Producto(String nombre, double precio, int stock){
     
         this.nombre = nombre;
@@ -20,6 +43,9 @@ public class Producto {
         this.stock = stock;
     }
 
+    /**
+     * Muestra información del producto en función de getters y lo imprime en pantalla. 
+     */
     public void mostrarInformacion() {
         System.out.println("Producto: " + getNombre() + " | Precio: " + getPrecio() + " | Stock: " + getStock());
     }
@@ -66,6 +92,6 @@ public class Producto {
     public void setStock(int stock) {
         this.stock = stock;
     }
-    
+
     
 }
