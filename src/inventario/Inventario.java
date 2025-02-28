94d7fb9fa6b5ad4c6eba047476e8ebba372987a6
Alejandro Caldi <alejandro.caldi@gmail.com>
2025-02-28 18:45:56 +0100

    Cambios en inventario y Jvados en todo el proyecto. Borrado de la main dummy.

--- src/inventario/Inventario.java
+++ src/inventario/Inventario.java
@@ -2,6 +2,13 @@ package inventario;
 
 import java.util.ArrayList;
 
+/**
+ * 
+ * @author Alejandro César Caldi
+ * @version 1.1.0 - modificada para adecuar cambios en Producto. 
+ * @since 2025-02-28
+ * @see Tarea par aunidad 4 de la materia Entornos de Desarrollo.
+ */
 public class Inventario {
     public ArrayList<Producto> listaProductos;
 
@@ -9,28 +16,35 @@ public class Inventario {
         listaProductos = new ArrayList<>();
     }
 
+    /**
+     * Agrega productos al Array<List> de la lista de productos. 
+     * @param p Instancia de producto a ser agregar a la lista.. 
+     */
     public void agregar(Producto p) {
         listaProductos.add(p);
     }
 
+    /**
+     * Recorre la lista de productos i va imprimiendo en pantalla cada item dentro de dichha lista 
+     */
     public void mostrar() {
         for (int i = 0; i < listaProductos.size(); i++) {
             listaProductos.get(i).mostrarInformacion();
         }
     }
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
+    
+        System.out.println("Hola");
         Inventario inventario = new Inventario();
-        Producto p1 = new Producto("Laptop", 1200.99, 10);
-        Producto p2 = new Producto("Mouse", 25.50, 50);
-        
+
+        Producto p1 = Producto.crearProducto("Laptop", 1200.99, 10);
+        Producto p2 = Producto.crearProducto("Mouse", 25.50, 50);
+
         inventario.agregar(p1);
         inventario.agregar(p2);
-        
+
         System.out.println("Inventario de productos:");
         inventario.mostrar();
-
-        p1.borrarDatos();
-        p2.borrarDatos();
     }
 }
